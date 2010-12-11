45063398ce1732a5cdcacd7c32a12795dcfc6ba0
Philippe Rathé <prathe@gmail.com>
2010-12-11 10:31:48 -0500

    route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,13 @@ ArnelaCa::Application.routes.draw do
     resources :collections
     resources :stores
     match 'about' => 'home#about', :as => 'about'
+    
+    # root
+    match '/' => 'home#index'
   end
+  
+  # Force the locale in the url
+  #match "/*url" => redirect("/fr/%{url}")
   
   # Sample resource route with options:
   #   resources :products do
@@ -49,11 +55,9 @@ ArnelaCa::Application.routes.draw do
   #     resources :products
   #   end
   
-  match '/:locale' => 'home#index'
-
-  # You can have the root of your site routed with "root"
+   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  #root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
