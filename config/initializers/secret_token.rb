b251d58423e63620c2dec1b4ac61282c3db6245c
Philippe Rathé <prathe@gmail.com>
2010-12-10 20:06:46 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArnelaCa::Application.config.secret_token = '70a5ab0c068812c41abef7ef2bf2e8352b141030f9a3d51f93a0d4b84a5cd63cc036df718425f10d71a3413fe7542bccfac7b0cc8c082c7da7e5c1b676caf716'
